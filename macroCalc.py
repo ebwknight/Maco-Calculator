8202ef78da192087714161f9f58c4325582b9ff0
ebwknight <evanbwknight@gmail.com>
2021-04-18 19:20:45 +0100

    first commit

---
+++ macroCalc.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 14:17:14 2021
+
+Whadup poopoo-head. Here's a macro calculator. Exports to excel for your financial ass
+
+@author: evanbwknight
+"""
+
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup as soup
+import requests
+import re
+from nutritionix import Nutritionix
+
+nix = Nutritionix(app_id="628fcca6", api_key="c3cd31c6207e1ce3b00a35615cba1295")
+
+print("Please enter your ingredients one at a time along with the ammount in grams. Enter nothing and press return twice when finished")
+ingredients = []
+meal = []
+try:
+    grub = input("First Ingredient: ")
+    ammount = input("Ammount (in grams): ")
+    while grub:
+        ingredients.append(tuple([str(grub), int(ammount)]))
+        grub = input("Next ingredient: ")
+        ammount = input("Ammount (in grams): ")
+             
+    
+    for ingredient in ingredients:
+        
+        food = nix.search(ingredient[0], results="0:1")
+        foodID = food.json()['hits'][0]['_id']
+        data = nix.item(id=foodID).json()
+        print(data)
+        
+        calories = ((data['nf_calories'] / data['nf_serving_weight_grams']) * ingredient[1])
+        fat = ((data['nf_total_fat'] / data['nf_serving_weight_grams']) * ingredient[1])
+        carbs = ((data['nf_total_carbohydrate'] / data['nf_serving_weight_grams']) * ingredient[1])
+        protein = ((data['nf_protein'] / data['nf_serving_weight_grams']) * ingredient[1])
+        
+        meal.append([ingredient[0], round(calories,0), round(fat,0), round(carbs,0), round(protein,0)])
+        
+    totalCals = 0
+    totalFats = 0
+    totalCarbs = 0
+    totalProtein = 0
+    
+    for item in meal:
+        totalCals += item[1]
+        totalFats += item[2]
+        totalCarbs += item[3]
+        totalProtein += item[4]
+    
+    meal.append(['Totals', totalCals, totalFats, totalCarbs, totalProtein])
+    
+    mealDF = pd.DataFrame(meal, columns = ['Ingredient', 'Calories', 'Fats', 'Carbs', 'Protein'])
+    filename = input("Please enter a filename. Do not include a filetype (Example: lasagna, not lasagna.txt): ")
+    mealDF.to_excel('{}.xlsx'.format(filename), index = False)  
+
+except Exception as e:
+    print(e)
+    
+    

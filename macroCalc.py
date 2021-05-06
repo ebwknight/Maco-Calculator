72d063eafeb81b5991ad55bc13d81325a8f636a7
ebwknight <evanbwknight@gmail.com>
2021-05-06 18:00:19 +0100

    added sugars column and updated requirements.txt

--- macroCalc.py
+++ macroCalc.py
@@ -8,6 +8,7 @@ Whadup poopoo-head. Here's a macro calculator. Exports to excel for your financi
 """
 
 import pandas as pd
+import openpyxl
 import numpy as np
 from bs4 import BeautifulSoup as soup
 import requests
@@ -16,7 +17,7 @@ from nutritionix import Nutritionix
 
 nix = Nutritionix(app_id="628fcca6", api_key="c3cd31c6207e1ce3b00a35615cba1295")
 
-print("Please enter your ingredients one at a time along with the ammount in grams. Enter nothing and press return twice when finished")
+print("Instructions:\nPlease enter your ingredients one at a time followed by the ammount in grams when prompted.\nWhen finished, enter nothing for ingredient and grams and simply press enter twice")
 ingredients = []
 meal = []
 try:
@@ -40,7 +41,8 @@ try:
             fat = ((data['nf_total_fat'] / data['nf_serving_weight_grams']) * ingredient[1])
             carbs = ((data['nf_total_carbohydrate'] / data['nf_serving_weight_grams']) * ingredient[1])
             protein = ((data['nf_protein'] / data['nf_serving_weight_grams']) * ingredient[1])
-            meal.append([ingredient[0], round(calories,0), round(fat,0), round(carbs,0), round(protein,0)])
+            sugar = ((data['nf_sugars'] / data['nf_serving_weight_grams']) * ingredient[1])
+            meal.append([ingredient[0], round(calories,0), round(fat,0), round(carbs,0), round(protein,0), round(sugar,0)])
         else:
             print("No grams per serving data available for " + data['item_name'])
             print("Ingredient not appended to sheet")
@@ -50,16 +52,18 @@ try:
     totalFats = 0
     totalCarbs = 0
     totalProtein = 0
+    totalSugar = 0
     
     for item in meal:
         totalCals += item[1]
         totalFats += item[2]
         totalCarbs += item[3]
         totalProtein += item[4]
+        totalSugar += item[5]
     
-    meal.append(['Totals', totalCals, totalFats, totalCarbs, totalProtein])
+    meal.append(['Totals', totalCals, totalFats, totalCarbs, totalProtein, totalSugar])
     
-    mealDF = pd.DataFrame(meal, columns = ['Ingredient', 'Calories', 'Fats', 'Carbs', 'Protein'])
+    mealDF = pd.DataFrame(meal, columns = ['Ingredient', 'Calories', 'Fats', 'Carbs', 'Protein', 'Sugars'])
     filename = input("Please enter a filename. Do not include a filetype (Example: lasagna, not lasagna.txt): ")
     mealDF.to_excel('{}.xlsx'.format(filename), index = False)  
 
